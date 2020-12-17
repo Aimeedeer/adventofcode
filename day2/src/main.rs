2bc3ae043a133b846fc1bf5966a0436dc91f1f22
aimeedeer <aimeez@pm.me>
2020-12-17 16:49:33 -0600

    works

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,18 +7,20 @@ fn main() -> Result<()> {
     let reader = BufReader::new(file);
 
     let mut num: i32 = 0;
-
+    
     for line in reader.lines() {
 	let rules = line?;
 	let rules = rules.trim().split(' ').collect::<Vec<_>>();
 
-	let scope = rules[0].split('-').collect::<Vec<_>>();
-	let scope_from = scope[0].parse::<i32>()?;
-	let scope_until = scope[1].parse::<i32>()?;
-
 	let valid_char = rules[1].trim_end_matches(':').parse::<char>()?;
 	let password = &rules[2];
 
+	let indexes = rules[0].split('-').collect::<Vec<_>>();
+	let index_1 = indexes[0].parse::<usize>()?;
+	let index_2 = indexes[1].parse::<usize>()?;
+
+	// part 1
+	/*
 	let mut num_valid_char = 0;
 	for c in password.chars() {
 	    if c == valid_char {
@@ -26,13 +28,27 @@ fn main() -> Result<()> {
 	    }
 	}
 
-//	println!("valid_char: {}, count: {}, scope: {}-{}, password: {:?} ", valid_char, num_valid_char, scope_from, scope_until, password);
-		
-	if num_valid_char >= scope_from && num_valid_char <= scope_until {
+	if num_valid_char >= index_1 && num_valid_char <= index_2 {
 	    num += 1;
 	} else {
 	    continue;
-	}	
+	}
+	*/
+
+	// part 2
+	let index_1 = index_1 - 1;
+	let index_2 = index_2 - 1;
+
+	if password.chars().nth(index_1) == password.chars().nth(index_2) {
+	    continue;
+	}
+	if (password.chars().nth(index_1) == Some(valid_char))
+	    || (password.chars().nth(index_2) == Some(valid_char)) {
+		num += 1;
+	    } else {
+		continue;
+	    }
+
     }
     println!("{} valid passwords", num);
 
