4d77398c8ba8afde9f0afd1abebd346d1ddc797c
aimeedeer <aimeez@pm.me>
2020-12-23 22:47:50 -0600

    first round

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,34 +4,44 @@ use anyhow::Result;
 use anyhow::anyhow;
 use regex::Regex;
 
+/*
 #[derive(Debug)]
 struct PassportVec {
-    data: Vec<PassportData>,
+    passports: Vec<Passport>,
 }
+*/
 
 #[derive(Debug)]
-struct PassportData {
-    key: String,
-    value: String,
+struct Passport {
+    pub pid: String,
+    pub cid: u32,
+    pub eyr: u32,
+    pub byr: u32,
+    pub iyr: u32,
+    pub ecl: String,
+    pub hcl: String,
+    pub hgt: String,
 }
 
-impl PassportVec {
-    pub fn new() -> Self {
+impl Passport {
+    pub fn new(
+	pid: String,
+	cid: u32,
+	eyr: u32,
+	byr: u32,
+	iyr: u32,
+	ecl: String,
+	hcl: String,
+	hgt: String) -> Self {
 	Self {
-	    data: Vec::<PassportData>::new(),
-	}
-    }
-
-    pub fn push(&self, passport: PassportData) {
-
-    }
-}
-
-impl PassportData {
-    pub fn new() -> Self {
-	Self {
-	    key: String::new(),
-	    value: String::new(),
+	    pid,
+	    cid, 
+	    eyr,
+	    byr,
+	    iyr,
+	    ecl,
+	    hcl,
+	    hgt,
 	}
     }
 }
@@ -42,26 +52,67 @@ fn main() -> Result<()>{
     file.read_to_string(&mut buffer)?;
 
     let raw_passport_vec = &buffer.split("\n\n").collect::<Vec<_>>();
-    let re_passport = Regex::new(r"(([[:alpha:]]{3}):(#|\d|[[:alnum:]]+)([\s\n]))")?;
+//    let re_passport = Regex::new(r"(([[:alpha:]]{3}):(#|\d|[[:alnum:]]+)([\s\n]))")?;
+    
+    let mut passport_vec: Vec<Passport> = vec![];
 
-    let mut passport_vec = PassportVec::new();    
     for p in raw_passport_vec {
 	let raw_passport = p.trim().split(&[' ', '\n'][..]).collect::<Vec<_>>();
-	println!("{:#?}", &raw_passport);
+	
+	let mut pid = String::new();
+	let mut cid = 0;
+	let mut iyr = 0;
+	let mut eyr = 0;
+	let mut byr = 0;
+	let mut ecl = String::new();
+	let mut hcl = String::new();
+	let mut hgt = String::new();
 
-	for raw in raw_passport {
-	    let data = raw.parse::<PassportData>()?;
-	    
-	    let key = &data[1];
-	    let value = &data[2];
+	for raw_item in raw_passport {
+//	    dbg!(&raw_item);
+	    let item = raw_item.split(':').collect::<Vec<_>>();
 
-	    let passport = PassportData {
-		    key,
-		    value,
-		};
-	    passport_vec.push(passport);		
+	    match item[0] {
+		"pid" => {
+		    pid = item[1].to_string();
+//		    dbg!(&pid);
+		},
+		"cid" => {
+		    cid = item[1].parse::<u32>()?;
+//			   dbg!(cid);
+		},
+		"iyr" => { iyr = item[1].parse::<u32>()?; },
+		"eyr" => { eyr = item[1].parse::<u32>()?; },
+		"byr" => { byr = item[1].parse::<u32>()?; },
+		"ecl" => { ecl = item[1].to_string(); },
+		"hcl" => { hcl = item[1].to_string(); },
+		"hgt" => { hgt = item[1].to_string(); },
+		_ => {},
+	    };
 	}
+
+	let passport = Passport::new(pid, cid, iyr, eyr, byr, ecl, hcl, hgt);
+	passport_vec.push(passport);
     }
 
+    println!("Valid passports: {}", verify(passport_vec));
     Ok(())
+}
+
+fn verify(passport_vec: Vec<Passport>) -> u32 {
+    let mut num = 0;
+
+    for passport in passport_vec {
+	if &passport.pid != ""
+	    && passport.eyr != 0
+	    && passport.byr != 0
+	    && passport.iyr != 0
+	    && &passport.ecl != ""
+	    && &passport.hcl != ""
+	    && &passport.hgt != ""	    
+	{	    
+	    num += 1;
+	}
+    }
+    num
 }
