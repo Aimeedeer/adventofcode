40b2b817b829fc9b777d8f90490b265f9dbefe42
aimeedeer <aimeez@pm.me>
2020-12-26 17:19:40 -0600

    consistancy

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,45 +5,12 @@ use anyhow::anyhow;
 use regex::Regex;
 
 #[derive(Debug, PartialEq, Clone)]
-struct Year {
-    year: u32,
-}
-
-impl Year {
-    pub fn eyr(year: u32) -> Result<Year> {
-	if year >= 2020 && year <= 2030 {
-	    Ok(Year { year, })
-	} else {
-	    Err(anyhow!("invalid eyr: {}", year))
-	}
-    }
-    
-    pub fn iyr(year: u32) -> Result<Year> {
-	if year >= 2010 && year <= 2020 {
-	    Ok(Year { year, })
-	} else {
-	    Err(anyhow!("invalid iyr: {}", year))
-	}
-    }
-    
-    pub fn byr(year: u32) -> Result<Year> {
-	if year >= 1920 && year <= 2002 {
-	    Ok(Year { year, })
-	} else {
-	    Err(anyhow!("invalid byr: {}", year))
-	}
-    }
-}
-
- 
-
-#[derive(Debug, PartialEq, Clone)]
 struct Passport {
     pub pid: Option<String>,
     pub cid: Option<u32>,
-    pub	eyr: Option<Year>,
-    pub	byr: Option<Year>,
-    pub	iyr: Option<Year>,
+    pub	eyr: Option<u32>,
+    pub	byr: Option<u32>,
+    pub	iyr: Option<u32>,
     pub ecl: Option<String>,
     pub hcl: Option<String>,
     pub hgt: Option<String>,
@@ -53,9 +20,9 @@ impl Passport {
     pub fn new(
 	pid: Option<String>,
 	cid: Option<u32>,
-	eyr: Option<Year>,
-	byr: Option<Year>,
-	iyr: Option<Year>,
+	eyr: Option<u32>,
+	byr: Option<u32>,
+	iyr: Option<u32>,
 	ecl: Option<String>,
 	hcl: Option<String>,
 	hgt: Option<String>) -> Self {
@@ -106,25 +73,26 @@ fn main() -> Result<()>{
 	    let item = raw_item.split(':').collect::<Vec<_>>();
 
 	    match item[0] {
-		"pid" => { pid = pid_is_valid(item[1].to_string()).ok(); },
-		"cid" => { cid = Some(item[1].parse::<u32>()?); },
-		"eyr" => { eyr = Year::eyr(item[1].parse::<u32>()?).ok(); },
-		"byr" => { byr = Year::byr(item[1].parse::<u32>()?).ok(); },
-		"iyr" => { iyr = Year::iyr(item[1].parse::<u32>()?).ok(); },
-		"ecl" => { ecl = ecl_is_valid(item[1].to_string()).ok(); },
-		"hcl" => { hcl = hcl_is_valid(item[1].to_string()).ok(); },
-		"hgt" => { hgt = hgt_is_valid(item[1].to_string()).ok(); },
+		"pid" => { pid = validate_pid(item[1]).ok(); },
+		"cid" => { cid = validate_cid(item[1]).ok(); },
+		"eyr" => { eyr = validate_eyr(item[1]).ok(); },
+		"byr" => { byr = validate_byr(item[1]).ok(); },
+		"iyr" => { iyr = validate_iyr(item[1]).ok(); },
+		"ecl" => { ecl = validate_ecl(item[1]).ok(); },
+		"hcl" => { hcl = validate_hcl(item[1]).ok(); },
+		"hgt" => { hgt = validate_hgt(item[1]).ok(); },
 		_ => {},
 	    };
 	}
 
 	let passport = Passport::new(pid, cid, eyr, byr, iyr, ecl, hcl, hgt);
-
-//	dbg!(&passport);
+	if passport.is_valid() {
+	    println!("{}\n", p);
+	}
+	
 	passport_vec.push(passport);
     }
 
-    println!("hi &passport_vec.len(): {}", &passport_vec.len());
     println!("Valid passports: {}", verify(passport_vec));
 
     Ok(())
@@ -134,51 +102,94 @@ fn verify(passport_vec: Vec<Passport>) -> u32 {
     let mut num = 0;
     for passport in passport_vec {
 	if passport.is_valid() {
-	    println!("{:#?}", &passport);
-
 	    num += 1;
 	}
     }
     num
 }
 
-pub fn pid_is_valid(pid: String) -> Result<String> {
+pub fn validate_pid(pid: &str) -> Result<String> {
     let re = Regex::new(r"(\d{9})").unwrap();
-    let caps = re.captures(&pid).ok_or(anyhow!("invalid pid: {:?}", pid))?;
+    let caps = re.captures(pid).ok_or(anyhow!("invalid pid: {:?}", pid))?;
     let pid = caps[1].to_string();
 
     Ok(pid)
 }
 
-pub fn hcl_is_valid(hcl: String) -> Result<String> {
-    let re = Regex::new(r"#(([a-f]|[0-9]){6})").unwrap();
-    let caps = re.captures(&hcl).ok_or(anyhow!("invalid hcl: {:?}", hcl))?;
-    let hcl = caps[1].to_string();
+pub fn validate_cid(cid: &str) -> Result<u32> {
+    Ok(cid.parse::<u32>()?)
+}
+
+pub fn validate_eyr(year: &str) -> Result<u32> {
+    let re = Regex::new(r"(\d{4})").unwrap();
+    let caps = re.captures(year).ok_or(anyhow!("invalid year: {:?}", year))?;
+    let year = caps[1].parse::<u32>()?;
     
+    if year >= 2020 && year <= 2030 {
+	Ok(year)
+    } else {
+	Err(anyhow!("invalid eyr: {}", year))
+    }
+}
+
+pub fn validate_iyr(year: &str) -> Result<u32> {
+    let re = Regex::new(r"(\d{4})").unwrap();
+    let caps = re.captures(year).ok_or(anyhow!("invalid year: {:?}", year))?;
+    let year = caps[1].parse::<u32>()?;	
+
+    if year >= 2010 && year <= 2020 {
+	Ok(year)
+    } else {
+	Err(anyhow!("invalid iyr: {}", year))
+    }
+}
+
+pub fn validate_byr(year: &str) -> Result<u32> {
+    let re = Regex::new(r"(\d{4})").unwrap();
+    let caps = re.captures(year).ok_or(anyhow!("invalid year: {:?}", year))?;
+    let year = caps[1].parse::<u32>()?;
+    
+    if year >= 1920 && year <= 2002 {
+	Ok(year)
+    } else {
+	Err(anyhow!("invalid byr: {}", year))
+    }
+}
+
+
+pub fn validate_hcl(hcl: &str) -> Result<String> {
+    let re = Regex::new(r"#(([a-f]|[0-9]){6})").unwrap();
+    let caps = re.captures(hcl).ok_or(anyhow!("invalid hcl: {:?}", hcl))?;
+    let hcl = caps[1].to_string();
+
     Ok(hcl)
 }
 
-pub fn ecl_is_valid(ecl: String) -> Result<String> {
-    match ecl.as_ref() {
-	"amb" | "blu" |	"brn" |	"gry" |	"grn" |	"hzl" | "oth" => Ok(ecl),
+pub fn validate_ecl(ecl: &str) -> Result<String> {
+    match ecl {
+	"amb" | "blu" |	"brn" |	"gry" |	"grn" |	"hzl" | "oth" => Ok(ecl.to_string()),
 	_ => Err(anyhow!("invalid ecl: {:?}", ecl)),
     }
 }
 
-pub fn hgt_is_valid(hgt: String) -> Result<String> {
+pub fn validate_hgt(hgt: &str) -> Result<String> {
     let err = Err(anyhow!("invalid hgt: {:?}", hgt));
     let len = hgt.len();
-    let num = &hgt[0..(len - 2)].parse::<i32>()?;
-    
+    let num = hgt[0..(len - 2)].parse::<i32>()?;
+
     if hgt.ends_with("cm") {	
-	if *num >= 150 && *num <= 193 {
-	    Ok(hgt)
+	if num >= 150 && num <= 193 {
+	    //          dbg!(&hgt);
+
+	    Ok(hgt.to_string())
 	} else {
 	    err
 	}
     } else if hgt.ends_with("in") {
-	if *num >= 59 && *num <= 76 {
-	    Ok(hgt)
+	if num >= 59 && num <= 76 {
+	    //            dbg!(&hgt);
+	    
+	    Ok(hgt.to_string())
 	} else {
 	    err
 	}
