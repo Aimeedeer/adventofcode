b7b311489e00a3be422389d3866a4abcee6dc505
aimeedeer <aimeez@pm.me>
2020-12-26 22:00:51 -0600

    reorg

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,6 +5,49 @@ use anyhow::Result;
 use anyhow::anyhow;
 use regex::Regex;
 
+fn main() -> Result<()>{
+    let mut file = File::open("input.txt")?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
+
+    let raw_passport_vec = &buffer.split("\n\n").collect::<Vec<_>>();
+    let mut passport_vec: Vec<Passport> = vec![];
+    
+    for p in raw_passport_vec {
+	let raw_passport = p.trim().split(&[' ', '\n'][..]).collect::<Vec<_>>();
+
+	let mut new_passport = RawPassport::default();
+	
+	fn validate<T>(dest: &mut Option<T>,
+		       reference: &str,
+		       v: impl FnOnce(&str) -> Result<T>) {
+	    *dest = v(reference).ok();	    	    
+	}
+	
+	for raw_item in raw_passport {
+	    let item = raw_item.split(':').collect::<Vec<_>>();
+	    match item[0] {	
+		"pid" => validate(&mut new_passport.pid, item[1], validate_pid),
+		"cid" => validate(&mut new_passport.cid, item[1], validate_cid),
+		"eyr" => validate(&mut new_passport.eyr, item[1], validate_eyr),
+		"byr" => validate(&mut new_passport.byr, item[1], validate_byr),
+		"iyr" => validate(&mut new_passport.iyr, item[1], validate_iyr),
+		"ecl" => validate(&mut new_passport.ecl, item[1], validate_ecl),
+		"hcl" => validate(&mut new_passport.hcl, item[1], validate_hcl),
+		"hgt" => validate(&mut new_passport.hgt, item[1], validate_hgt),
+		_ => {},
+	    };
+	}
+
+	if let Some(passport) = Passport::new(new_passport){
+	    passport_vec.push(passport);
+	}	
+    }
+
+    println!("Valid passports: {}", passport_vec.len());
+    Ok(())
+}
+
 #[derive(Debug, PartialEq, Clone, Default)]
 struct RawPassport {
     pid: Option<String>,
@@ -60,68 +103,6 @@ impl Passport {
     }
 }
 
-fn main() -> Result<()>{
-    let mut file = File::open("input.txt")?;
-    let mut buffer = String::new();
-    file.read_to_string(&mut buffer)?;
-
-    let raw_passport_vec = &buffer.split("\n\n").collect::<Vec<_>>();
-    let mut passport_vec: Vec<Passport> = vec![];
-    
-    for p in raw_passport_vec {
-	let raw_passport = p.trim().split(&[' ', '\n'][..]).collect::<Vec<_>>();
-
-	let mut new_passport = RawPassport::default();
-	
-	fn validate<T>(dest: &mut Option<T>,
-		       reference: &str,
-		       v: impl FnOnce(&str) -> Result<T>) {
-	    *dest = v(reference).ok();	    	    
-	}
-	
-	for raw_item in raw_passport {
-	    let item = raw_item.split(':').collect::<Vec<_>>();
-	    match item[0] {	
-		"pid" => validate(&mut new_passport.pid, item[1], validate_pid),
-		"cid" => validate(&mut new_passport.cid, item[1], validate_cid),
-		"eyr" => validate(&mut new_passport.eyr, item[1], validate_eyr),
-		"byr" => validate(&mut new_passport.byr, item[1], validate_byr),
-		"iyr" => validate(&mut new_passport.iyr, item[1], validate_iyr),
-		"ecl" => validate(&mut new_passport.ecl, item[1], validate_ecl),
-		"hcl" => validate(&mut new_passport.hcl, item[1], validate_hcl),
-		"hgt" => validate(&mut new_passport.hgt, item[1], validate_hgt),
-		_ => {},
-	    };
-	}
-
-	if let Some(passport) = Passport::new(new_passport){
-	    passport_vec.push(passport);
-	}	
-    }
-
-    println!("Valid passports: {}", passport_vec.len());
-
-    Ok(())
-}
-
-fn parse_and_capture<T: FromStr>(rule: &str, input: &str, msg: &str) -> Result<T> {
-    let re = Regex::new(rule).unwrap();
-    let caps = re.captures(input).ok_or(anyhow!("invalid {}: {}", msg, input))?;
-
-    let output = caps[1].parse::<T>().map_err(|_| anyhow!("error parsing {}: {}", msg, input))?;
-    Ok(output)
-}
-
-fn validate_year(year: &str, least: u32, most: u32, msg: &str) -> Result<u32> {
-    let year = parse_and_capture(r"(\d{4})", year, "year")?;
-    
-    if year >= least && year <= most {
-	Ok(year)
-    } else {
-	Err(anyhow!("invalid {}: {}", msg, year))
-    }
-}
-
 fn validate_pid(pid: &str) -> Result<String> {
     parse_and_capture(r"(\d{9})", pid, "pid")
 }
@@ -172,3 +153,20 @@ fn validate_hgt(hgt: &str) -> Result<String> {
     }
 }
 
+fn parse_and_capture<T: FromStr>(rule: &str, input: &str, msg: &str) -> Result<T> {
+    let re = Regex::new(rule).unwrap();
+    let caps = re.captures(input).ok_or(anyhow!("invalid {}: {}", msg, input))?;
+
+    let output = caps[1].parse::<T>().map_err(|_| anyhow!("error parsing {}: {}", msg, input))?;
+    Ok(output)
+}
+
+fn validate_year(year: &str, least: u32, most: u32, msg: &str) -> Result<u32> {
+    let year = parse_and_capture(r"(\d{4})", year, "year")?;
+    
+    if year >= least && year <= most {
+	Ok(year)
+    } else {
+	Err(anyhow!("invalid {}: {}", msg, year))
+    }
+}
