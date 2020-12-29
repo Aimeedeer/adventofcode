c36594275c297e7edae16fc0700ca5586be55d9a
aimeedeer <aimeez@pm.me>
2020-12-28 18:19:37 -0600

    part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -29,47 +29,46 @@ fn main() -> Result<()>{
 }
 
 fn get_row_id(input: &str) -> Result<u32> {
-    let mut current_rows = create_vec(128);
+    let mut range = create_vec(128);
     
     for c in input.chars() {
-	let len = current_rows.len();
-	let (front, back) = current_rows.split_at(len/2);
+	let len = range.len();
+	let (front, back) = range.split_at(len/2);
 
 	match c {
 	    'F' => {
-		current_rows = front.to_vec();
+		range = front.to_vec();
 	    },
 	    'B' => {
-		current_rows = back.to_vec();
+		range = back.to_vec();
 	    },
 	    _ => { anyhow!("get row id failed"); },
 	}
-	dbg!(&current_rows);
     }
     
-    let row_id = current_rows[0];
+    let row_id = range[0];
     Ok(row_id)
 }
 
 fn get_column_id(input: &str) -> Result<u32> {
-    let mut current_columns = create_vec(8);
+    let mut range = create_vec(8);
 
     for c in input.chars() {
-	let len = current_columns.len();
-	let (left, right) = current_columns.split_at(len/2);
+	let len = range.len();
+	let (left, right) = range.split_at(len/2);
 
 	match c {
 	    'L' => {
-		current_columns = left.to_vec();
+		range = left.to_vec();
 	    },
 	    'R' => {
-		current_columns = right.to_vec();
+		range = right.to_vec();
 	    }
 	    _ => { anyhow!("get column id failed"); },
 	}
     }
 
-    let column_id = current_columns[0];    
+    let column_id = range[0];    
     Ok(column_id)
 }
 
