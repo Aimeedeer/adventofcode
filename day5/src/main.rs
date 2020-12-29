c2703b31a74a4d1d1c40de4560e84d8a8f04f1ba
aimeedeer <aimeez@pm.me>
2020-12-28 18:12:02 -0600

    part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() -> Result<()>{
-    let mut file = File::open("input.txt")?;
+    let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
     let mut seat_id_vec: Vec<u32> = Vec::new();
@@ -20,17 +20,16 @@ fn main() -> Result<()>{
 	let seat_id = row_id * 8 + column_id;
 	seat_id_vec.push(seat_id);
     }
+    seat_id_vec.sort();
 
-    // todo: sort seat_id_vec and find the highest num
-    let higest_seat_id = get_highest_seat_id(&seat_id_vec);
+    let higest_seat_id = seat_id_vec[seat_id_vec.len() - 1];
     println!("The highest seat ID: {}", higest_seat_id);
 
     Ok(())
 }
 
 fn get_row_id(input: &str) -> Result<u32> {
-    let mut row_id = 0;
-    let mut current_rows = create_rows();
+    let mut current_rows = create_vec(128);
     
     for c in input.chars() {
 	let len = current_rows.len();
@@ -45,31 +44,40 @@ fn get_row_id(input: &str) -> Result<u32> {
 	    },
 	    _ => { anyhow!("get row id failed"); },
 	}
-	
 	dbg!(&current_rows);
     }
     
+    let row_id = current_rows[0];
     Ok(row_id)
 }
 
 fn get_column_id(input: &str) -> Result<u32> {
-    let mut column_id = 0;
-    
+    let mut current_columns = create_vec(8);
+
+    for c in input.chars() {
+	let len = current_columns.len();
+	let (left, right) = current_columns.split_at(len/2);
+
+	match c {
+	    'L' => {
+		current_columns = left.to_vec();
+	    },
+	    'R' => {
+		current_columns = right.to_vec();
+	    }
+	    _ => { anyhow!("get column id failed"); },
+	}
+    }
+
+    let column_id = current_columns[0];    
     Ok(column_id)
 }
 
-fn get_highest_seat_id(list: &Vec<u32>) -> u32 {
-    let mut seat_id = 0;
-
-    seat_id
-}
-
-fn create_rows() -> Vec<u32> {
-    let mut rows: Vec<u32> = Vec::with_capacity(128);
+fn create_vec(len: u32) -> Vec<u32> {
+    let mut rows: Vec<u32> = Vec::with_capacity(len as usize);
     
-    for i in 0..128 {
+    for i in 0..len {
         rows.push(i);
     }
-    dbg!(&rows);
     rows
 }
