703a8209eea737550273ed5987e61e456d249e8f
aimeedeer <aimeez@pm.me>
2021-01-10 12:27:03 -0600

    part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::io::prelude::*;
 
 fn main() -> Result<()> {
     let mut file = File::open("input.txt")?;
@@ -9,22 +9,44 @@ fn main() -> Result<()> {
 
     let groups = &buffer.split("\n\n").collect::<Vec<_>>();
 
-    let mut num = 0;
-    for g in groups {
-        let g = g.replace("\n", "");
-//        println!("after: {:?}", &g);
+    let mut num1 = 0;
+    let mut num2 = 0;
 
+    for g in groups {
+        let g1 = g.replace("\n", "");
+        let g2 = g.trim().split('\n').collect::<Vec<_>>();
+
+        // part 1
         let mut question_vec = Vec::new();
-        for c in g.chars() {
+        for c in g1.chars() {
             if !question_vec.contains(&c) {
                 question_vec.push(c);
             }
         }
-        num += question_vec.len();
-        
-        println!("num: {}", num);
-        println!("---------------------------------------");
+        let question_num = question_vec.len();
+        num1 += question_num;
+
+        // part 2
+        let mut question_vec_2 = Vec::new();
+        for c in question_vec.iter() {
+            let mut is_answered = true;
+            for q in g2.iter() {
+                if !q.contains(*c) {
+                    is_answered = false;
+                    break;
+                }
+            }
+            if is_answered == true {
+                question_vec_2.push(c);
+            }
+        }
+
+        let question_num_2 = question_vec_2.len();
+        num2 += question_num_2;
     }
+
+    println!("num1: {}", num1);
+    println!("num2: {}", num2);
     
     Ok(())
 }
